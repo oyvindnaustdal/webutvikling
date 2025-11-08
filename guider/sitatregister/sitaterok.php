c9933efe85b775b16bc551c63814924fe23484b6
oyvindnaustdal <oyvind.naustdal@gmail.com>
2025-11-08 15:52:36 +0100

    Add files via upload

---
+++ guider/sitatregister/sitaterok.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<head>
+    <title>Sitat lagt til</title>
+    <meta charset="utf-8" />
+    <link rel ="stylesheet" type="text/css" href="stil.css">
+</head>
+    <body>
+        <h1>Sitatet er lagt til i databasen</h1>
+        <p>Du kan no sjå ein oversikt over sitata
+        <a href="sitateravansert.php">her</a></p>
+    </body>
+</html>
